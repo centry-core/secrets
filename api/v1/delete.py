6c9b4a4ec1d868f9a99b522c1d10b2199b6b21f9
aspect13 <ad13box@gmail.com>
2022-04-28 18:26:17 +0300

    fixing secrets rpc and tools

---
+++ api/v1/delete.py
@@ -0,0 +1,23 @@
+from typing import Tuple
+
+from flask import request, make_response
+from flask_restful import Resource
+
+from tools import secrets_tools
+
+
+class API(Resource):
+    def __init__(self, module):
+        self.module = module
+
+    def post(self, project_id: int) -> Tuple[dict, int]:
+        project = self.module.context.rpc_manager.call.project_get_or_404(project_id)
+        data = request.json
+        secrets = secrets_tools.get_project_secrets(project.id)
+        for secret in data.get('secrets', []):
+            try:
+                del secrets[secret]
+            except KeyError:
+                ...
+        secrets_tools.set_project_secrets(project.id, secrets)
+        return make_response({"message": "deleted"}, 200)
--- api/v1/secret.py
+++ api/v1/secret.py
@@ -1,61 +1,49 @@
 from typing import Tuple
 
+from flask import make_response, request
 from flask_restful import Resource
 
-from ...connectors.secrets import get_project_secrets, set_project_secrets, get_project_hidden_secrets, \
-    set_project_hidden_secrets
-
-
-# from ...shared.utils.restApi import RestResource
-# from ...shared.utils.api_utils import build_req_parser
+from tools import secrets_tools
 
 
 class API(Resource):  # pylint: disable=C0111
-    # post_rules = (
-    #     dict(name="secret", type=str, required=True, default=None, location="json"),
-    # )
-    #
-    # def __init__(self):
-    #     super().__init__()
-    #     self.__init_req_parsers()
-    #
-    # def __init_req_parsers(self):
-    #     self._parser_post = build_req_parser(rules=self.post_rules)
+    def __init__(self, module):
+        self.module = module
 
     def get(self, project_id: int, secret: str) -> Tuple[dict, int]:  # pylint: disable=R0201,C0111
         # Check project_id for validity
-        project = self.rpc.project_get_or_404(project_id)
+        project = self.module.context.rpc_manager.call.project_get_or_404(project_id)
         # Get secret
-        secrets = get_project_secrets(project.id)
-        _secret = secrets.get(secret) if secrets.get(secret) else get_project_hidden_secrets(project.id).get(secret)
-        return {"secret": _secret}, 200
+        secrets = secrets_tools.get_project_secrets(project.id)
+        _secret = secrets.get(secret) if secrets.get(secret) else secrets_tools.get_project_hidden_secrets(project.id).get(secret)
+        return make_response({"secret": _secret}, 200)
 
     def post(self, project_id: int, secret: str) -> Tuple[dict, int]:  # pylint: disable=C0111
-        data = self._parser_post.parse_args()
+        data = request.json
         # Check project_id for validity
-        project = self.rpc.project_get_or_404(project_id)
+        project = self.module.context.rpc_manager.call.project_get_or_404(project_id)
         # Set secret
-        secrets = get_project_secrets(project.id)
+        secrets = secrets_tools.get_project_secrets(project.id)
         secrets[secret] = data["secret"]
-        set_project_secrets(project.id, secrets)
-        return {"message": f"Project secret was saved"}, 200
+        secrets_tools.set_project_secrets(project.id, secrets)
+        return make_response({"message": f"Project secret was saved"}, 200)
 
     def put(self, project_id: int, secret: str) -> Tuple[dict, int]:  # pylint: disable=C0111
         # Check project_id for validity
-        project = self.rpc.project_get_or_404(project_id)
+        project = self.module.context.rpc_manager.call.project_get_or_404(project_id)
         # Set secret
-        secrets = get_project_secrets(project.id)
-        hidden_secrets = get_project_hidden_secrets(project.id)
+        secrets = secrets_tools.get_project_secrets(project.id)
+        hidden_secrets = secrets_tools.get_project_hidden_secrets(project.id)
         hidden_secrets[secret] = secrets[secret]
         secrets.pop(secret, None)
-        set_project_secrets(project.id, secrets)
-        set_project_hidden_secrets(project.id, hidden_secrets)
-        return {"message": f"Project secret was moved to hidden secrets"}, 200
+        secrets_tools.set_project_secrets(project.id, secrets)
+        secrets_tools.set_project_hidden_secrets(project.id, hidden_secrets)
+        return make_response({"message": f"Project secret was moved to hidden secrets"}, 200)
 
     def delete(self, project_id: int, secret: str) -> Tuple[dict, int]:  # pylint: disable=C0111
-        project = self.rpc.project_get_or_404(project_id)
-        secrets = get_project_secrets(project.id)
+        project = self.module.context.rpc_manager.call.project_get_or_404(project_id)
+        secrets = secrets_tools.get_project_secrets(project.id)
         if secret in secrets:
             del secrets[secret]
-        set_project_secrets(project.id, secrets)
-        return {"message": "deleted"}, 200
+        secrets_tools.set_project_secrets(project.id, secrets)
+        return make_response({"message": "deleted"}, 204)
